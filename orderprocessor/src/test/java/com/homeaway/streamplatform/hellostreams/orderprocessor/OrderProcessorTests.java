02617c20d4ad40b94746dbfe626c716dbc051b8c
Rene X. Parra <552515+neoword@users.noreply.github.com>
2019-04-01 19:32:32 -0500

    setup valid and invalid order processing tests

--- orderprocessor/src/test/java/com/homeaway/streamplatform/hellostreams/orderprocessor/OrderProcessorTests.java
+++ orderprocessor/src/test/java/com/homeaway/streamplatform/hellostreams/orderprocessor/OrderProcessorTests.java
@@ -2,7 +2,6 @@ package com.homeaway.streamplatform.hellostreams.orderprocessor;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.graphql.spring.boot.test.GraphQLResponse;
 import com.graphql.spring.boot.test.GraphQLTest;
@@ -23,6 +22,7 @@ import static org.junit.Assert.*;
 @Slf4j
 public class OrderProcessorTests {
 
+    private static final String INVALID_CUSTOMER_ID = "35442a6a-ad7a-4e72-8af9-5611254306a6";
     @Resource
     private GraphQLTestTemplate graphQLTemplate;
 
@@ -40,7 +40,27 @@ public class OrderProcessorTests {
         JsonNode jsonNode = response.readTree();
         assertThat(response.getStatusCode().value(), is(200));
         assertThat(jsonNode.get("errors"), is(nullValue()));
-        assertThat(JsonNode.get("data"), is(notNullValue()));
+        assertThat(jsonNode.get("data"), is(notNullValue()));
+        assertThat(jsonNode.get("data").get("placeOrder"), is(notNullValue()));
         assertThat(response.get("data.placeOrder.customerId", String.class), is(CustomerDao.CUSTOMER_TEST_ID));
+    }
+
+    @Test
+    public void placeOrderInvalidCustomer() throws Exception {
+        // setup variables
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode varsNode = mapper.createObjectNode();
+        varsNode.put("customerId", INVALID_CUSTOMER_ID);
+        varsNode.put( "item", "Latte");
+
+        // place Order
+        GraphQLResponse response = graphQLTemplate.perform("placeOrder.mutation", varsNode);
+        log.info("statusCode={}", response.getStatusCode());
+        JsonNode jsonNode = response.readTree();
+        assertThat(response.getStatusCode().value(), is(200));
+        assertThat(jsonNode.get("errors"), is(notNullValue()));
+        assertThat(jsonNode.get("data").isNull(), is(true));
+        assertThat(response.get("errors[0].message", Object.class).toString(), containsString(INVALID_CUSTOMER_ID));
+        assertThat(response.get("errors[0].message", Object.class).toString(), containsString("is non-existent"));
     }
 }
