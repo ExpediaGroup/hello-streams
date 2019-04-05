13ded9c423a38a70a31b43d1528547b55496e807
Matthew Cale <mattc41190@gmail.com>
2019-04-05 12:28:47 -0500

    dirty

--- orderprocessor/src/test/java/com/homeaway/streamplatform/hellostreams/orderprocessor/SpringBootTests.java
+++ orderprocessor/src/test/java/com/homeaway/streamplatform/hellostreams/orderprocessor/SpringBootTests.java
@@ -2,13 +2,16 @@ package com.homeaway.streamplatform.hellostreams.orderprocessor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT )
+@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
 public class SpringBootTests {
-
+	@Autowired
+	private TestRestTemplate restTemplate;
 	@Test
 	public void contextLoads() {
 	}
